3c31ec4b91edb9a9ae3ea10b45229e2141377d9a
Yaaseen <yaaseendav@gmail.com>
2022-06-21 08:54:00 +0200

    button color

--- src/App.vue
+++ src/App.vue
@@ -71,7 +71,19 @@
           @click="setActive(active - 1)"
           v-if="active !== 0"
         >
-          <i class="carousel-control-prev-icon blue" aria-hidden="true"></i>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="19"
+            height="19"
+            fill="#507ad5"
+            class="bi bi-chevron-left"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+            />
+          </svg>
         </button>
         <button
           class="next-btn-right"
@@ -81,8 +93,19 @@
           data-bs-slide="next"
           v-if="active !== total"
         >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="19"
+            height="19"
+            fill="#507ad5"
+            class="bi bi-chevron-right"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+            />
+          </svg>
         </button>
       </div>
     </div>
@@ -154,9 +177,7 @@ export default {
 body {
   overflow-x: hidden;
 }
-.blue {
-  color: blue;
-}
+
 #menuToggle {
   display: block;
   position: fixed;
